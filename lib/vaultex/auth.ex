f1cc141f26d60eb2d9812283f9521c20906537df
Thanya Nitithatsanakul <ammbot@gmail.com>
2017-01-30 21:21:43 +0700

    update code to avoid warning elixir 1.4

--- lib/vaultex/auth.ex
+++ lib/vaultex/auth.ex
@@ -16,13 +16,13 @@ defmodule Vaultex.Auth do
   end
 
   def handle(:ec2, {role}, state) do
-    pkcs7 = get_aws_pkcs7
-    nonce = get_nonce
+    pkcs7 = get_aws_pkcs7()
+    nonce = get_nonce()
     request(:post, "#{state.url}auth/aws-ec2/login", %{"pkcs7": pkcs7, "nonce": nonce, "role": role}, [{"Content-Type", "application/json"}])
     |> handle_response(state)
   end
 
-  def get_aws_pkcs7() do
+  def get_aws_pkcs7 do
     result = request(:get, "http://169.254.169.254/latest/dynamic/instance-identity/pkcs7")
     case result do
       {:ok, response} -> response.body
@@ -49,7 +49,7 @@ defmodule Vaultex.Auth do
     HTTPoison.request(:get, url)
   end
 
-  defp get_nonce() do
+  defp get_nonce do
     System.get_env("VAULT_NONCE") || Application.get_env(:vaultex, :nonce) || ""
   end
 
